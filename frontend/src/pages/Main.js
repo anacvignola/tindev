ee6848817fd842355953a19cbbe0bad7d8fed976
anacvignola <anacvignola@gmail.com>
2019-09-20 13:09:06 -0300

    add Link

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Main.css';
 
 import api from '../services/api';
@@ -38,30 +39,33 @@ export default function Main({ match }) {
 
     return (
         <div className="main-container">
-            <img src={logo} alt="Tindev" />
-                { users.lenght > 0 ? (
-                    <ul>
-                        {users.map(user => ( 
-                            <li key={user._id}>
-                                <img src={user.avatar} alt={user.name} />
-                                <footer>
-                                    <strong>{user.name}</strong>
-                                    <p>{user.bio}</p>
-                                </footer>
-                                <div className="buttons">
-                                    <button type="button" onClick={() => handleDislike(user._id)}>
-                                        <img src={dislike} alt="Dislike" />
-                                    </button>
-                                    <button type="button" onClick={() => handleLike(user._id)}>
-                                        <img src={like} alt="Like" />
-                                    </button>
-                                </div>
-                            </li>
-                            ))}
-                        </ul>
-                ) : (
-                    <div className="empty">Acabou :(</div>
-                ) }
+            <Link to="/">
+                <img src={logo} alt="Tindev" />
+            </Link>
+            { users.length > 0 ? (
+            <ul>  
+                {users.map(user => (
+                    <li key={user._id}>
+                    <img src={user.avatar} alt={user.name} />
+                    <footer>
+                        <strong>{user.name}</strong>
+                        <p>{user.bio}</p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button" onClick={() => handleDislike(user._id)}>
+                        <img src={dislike} alt="Dislike" />
+                        </button>
+                        <button type="button" onClick={() => handleLike(user._id)}>
+                        <img src={like} alt="Like" />
+                        </button>
+                    </div>
+                    </li>
+                ))}
+            </ul>
+            ) : (
+                <div className="empty">Acabou :(</div>
+            ) }
         </div>
     )    
 }
